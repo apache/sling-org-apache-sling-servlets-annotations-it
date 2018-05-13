28b8b20cf3418ebd7cd954ec41e9148745d37108
Konrad Windszus <kwin@apache.org>
2018-05-13 12:10:15 +0200

    SLING-7624 initial IT for servlet annotations

---
+++ src/main/java/org/apache/sling/servlets/annotations/testservlets/ResourceTypeBoundServletWithMethods.java
@@ -0,0 +1,58 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.sling.servlets.annotations.testservlets;
+
+import java.io.IOException;
+
+import javax.servlet.Servlet;
+import javax.servlet.ServletException;
+
+import org.apache.sling.api.SlingHttpServletRequest;
+import org.apache.sling.api.SlingHttpServletResponse;
+import org.apache.sling.api.servlets.ServletResolverConstants;
+import org.apache.sling.api.servlets.SlingAllMethodsServlet;
+import org.apache.sling.servlets.annotations.SlingServletByResourceType;
+import org.osgi.service.component.annotations.Component;
+
+@Component(service=Servlet.class, property = { 
+        ServletResolverConstants.SLING_SERVLET_RESOURCE_TYPES + "=/apps/sling/testservlets/ResourceTypeBoundServletWithMethods",
+        ServletResolverConstants.SLING_SERVLET_METHODS + "=PUT",
+        ServletResolverConstants.SLING_SERVLET_METHODS + "=POST"
+      })
+@SlingServletByResourceType(sling_servlet_resourceTypes="/apps/sling/testservlets/ResourceTypeBoundServletWithMethods", sling_servlet_methods= {"PUT", "POST"})
+public class ResourceTypeBoundServletWithMethods extends SlingAllMethodsServlet {
+
+    /**
+     * 
+     */
+    private static final long serialVersionUID = 1L;
+
+    @Override
+    protected void doGet(SlingHttpServletRequest request, SlingHttpServletResponse response) throws ServletException, IOException {
+        response.setStatus(555);
+    }
+
+    @Override
+    protected void doPut(SlingHttpServletRequest request, SlingHttpServletResponse response) throws ServletException, IOException {
+        response.setStatus(555);
+    }
+    
+    @Override
+    protected void doPost(SlingHttpServletRequest request, SlingHttpServletResponse response) throws ServletException, IOException {
+        response.setStatus(555);
+    }
+}
---
+++ src/test/java/org/apache/sling/servlets/annotations/ServletRegistrationIT.java
@@ -0,0 +1,139 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.sling.servlets.annotations;
+
+import java.io.File;
+import java.io.UnsupportedEncodingException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.util.Collections;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.TimeoutException;
+
+import org.apache.http.entity.StringEntity;
+import org.apache.sling.testing.clients.ClientException;
+import org.apache.sling.testing.clients.osgi.OsgiConsoleClient;
+import org.apache.sling.testing.junit.rules.SlingRule;
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Ignore;
+import org.junit.Rule;
+import org.junit.Test;
+
+public class ServletRegistrationIT {
+    
+    @Rule
+    public SlingRule methodRule = new SlingRule();
+    
+    public static OsgiConsoleClient CLIENT;
+    
+    private static final String BUNDLE_SYMBOLICNAME = "sling-org-apache-sling-servlets-annotations-it";
+    private static final long BUNDLE_START_TIMEOUT = TimeUnit.SECONDS.toMillis(10);
+    private static final long SERVICE_START_TIMEOUT = TimeUnit.SECONDS.toMillis(60);
+    
+    @BeforeClass
+    public static void setupOnce() throws ClientException, InterruptedException, TimeoutException, URISyntaxException {
+        String baseUrl = System.getProperty("baseUrl");
+        if (baseUrl == null) {
+            throw new IllegalArgumentException("IT must be started with environment variable 'baseUrl' set");
+        }
+        URI url = new URI(baseUrl);
+        CLIENT = new OsgiConsoleClient(url, "admin", "admin");
+        
+        String bundleFile = System.getProperty("bundleFile");
+        if (bundleFile == null) {
+            throw new IllegalArgumentException("IT must be started with environment variable 'bundleFile' set");
+        }
+        // deploy bundle to server
+        File file = new File(bundleFile);
+        if (!file.exists()) {
+            throw new IllegalArgumentException("Test bundle file in " + file + " does not exist!");
+        }
+        CLIENT.waitInstallBundle(file, true, -1, BUNDLE_START_TIMEOUT, 500);
+        // wait until the server is fully started
+        CLIENT.waitExists("/index.html", SERVICE_START_TIMEOUT, 500);
+        
+        CLIENT.waitServiceRegistered("javax.servlet.Servlet", BUNDLE_SYMBOLICNAME, SERVICE_START_TIMEOUT, 500);
+    }
+    
+    @AfterClass
+    public static void tearDownOnce() throws ClientException {
+        //CLIENT.uninstallBundle(BUNDLE_SYMBOLICNAME);
+    }
+
+    @Test
+    public void testPathBoundServlet() throws ClientException {
+        CLIENT.doGet("/bin/PathBoundServlet", 555);
+        CLIENT.doGet("/bin/PathBoundServlet.with.some.selector.and.extension", 555);
+    }
+
+    @Test
+    @Ignore("Prefix is ignored!")
+    public void testPathBoundServletWithPrefix() throws ClientException {
+        CLIENT.doGet("/bin/PathBoundServletWithPrefix", 555);
+    }
+
+    @Test
+    public void testResourceTypeBoundServlet() throws ClientException, UnsupportedEncodingException {
+        CLIENT.doGet("/content/servlettest/resourceTypeBoundServlet", 555);
+        CLIENT.doGet("/content/servlettest/resourceTypeBoundServlet.html", 555);
+        CLIENT.doGet("/content/servlettest/resourceTypeBoundServlet.json", 555);
+        // only GET and HEAD are supposed to be working
+        CLIENT.doPut("/content/servlettest/resourceTypeBoundServlet.json", new StringEntity("some text"), Collections.emptyList(), 201);
+    }
+
+    @Test
+    @Ignore("Prefix is not working somehow")
+    public void testResourceTypeBoundServletWithPrefix() throws ClientException, UnsupportedEncodingException {
+        CLIENT.doGet("/content/servlettest/resourceTypeBoundServletWithPrefix", 403);
+        CLIENT.doGet("/content/servlettest/resourceTypeBoundServletWithPrefix.html", 555);
+        CLIENT.doGet("/content/servlettest/resourceTypeBoundServletWithPrefix.json", 555);
+        // only GET and HEAD are supposed to be working
+        CLIENT.doPut("/content/servlettest/resourceTypeBoundServletWithPrefix.json", new StringEntity("some text"), Collections.emptyList(), 201);
+    }
+
+    @Test
+    public void testResourceTypeBoundServletWithExtension() throws ClientException, UnsupportedEncodingException {
+        CLIENT.doGet("/content/servlettest/resourceTypeBoundServletWithExtension", 403); // without extension is a index listing, which is forbidden by default
+        CLIENT.doGet("/content/servlettest/resourceTypeBoundServletWithExtension.html", 200); // DEFAULT GET Servlet
+        CLIENT.doGet("/content/servlettest/resourceTypeBoundServletWithExtension.ext1", 555);
+        CLIENT.doGet("/content/servlettest/resourceTypeBoundServletWithExtension.ext2", 555);
+        // only GET and HEAD are supposed to be working
+        CLIENT.doPut("/content/servlettest/resourceTypeBoundServletWithExtension.ext2", new StringEntity("some text"), Collections.emptyList(), 201);
+    }
+
+    @Test
+    public void testResourceTypeBoundServletWithSelectors() throws ClientException, UnsupportedEncodingException {
+        CLIENT.doGet("/content/servlettest/resourceTypeBoundServletWithSelectors.someext", 404);
+        CLIENT.doGet("/content/servlettest/resourceTypeBoundServletWithSelectors.selector1.someext", 404);
+        CLIENT.doGet("/content/servlettest/resourceTypeBoundServletWithSelectors.selector3.someext", 555);
+        CLIENT.doGet("/content/servlettest/resourceTypeBoundServletWithSelectors.selector1.selector2.someext", 555);
+        // some non-registered selector as first selector
+        CLIENT.doGet("/content/servlettest/resourceTypeBoundServletWithSelectors.someotherselector.selector1.selector2.someext", 404);
+        // some non-registered selector as last selector
+        CLIENT.doGet("/content/servlettest/resourceTypeBoundServletWithSelectors.selector1.selector2.someotherselector.someext", 555);
+        // only GET and HEAD are supposed to be working
+        CLIENT.doPut("/content/servlettest/resourceTypeBoundServletWithSelectors.selector3.someext", new StringEntity("some text"), Collections.emptyList(), 201);
+    }
+
+    @Test
+    public void testResourceTypeBoundServletWithMethods() throws ClientException, UnsupportedEncodingException {
+        CLIENT.doGet("/content/servlettest/resourceTypeBoundServletWithMethods.someext", 404); // DEFAULT Get not triggered due to weird extension
+        CLIENT.doPut("/content/servlettest/resourceTypeBoundServletWithMethods.someext", new StringEntity("some text"), Collections.emptyList(), 555);
+        CLIENT.doPost("/content/servlettest/resourceTypeBoundServletWithMethods.someext", new StringEntity("some text"), Collections.emptyList(), 555);
+    }
+}
